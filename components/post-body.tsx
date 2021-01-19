ddbc9498bf341af5c92d2d5c91e43c56f6721cbb
linke.wang <wangyu3@dxy.cn>
2021-01-19 16:59:36 +0800

    chore: 代码块增加高亮

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,13 +1,25 @@
 import markdownStyles from './markdown-styles.module.css'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/atom-one-dark.css';
+import { useEffect, useRef } from 'react';
 
 type Props = {
   content: string
 }
 
 const PostBody = ({ content }: Props) => {
+  const ref = useRef(null)
+  useEffect(() => {
+    if (!!ref && !!ref.current) {
+      document.querySelectorAll('pre code').forEach((block) => {
+        hljs.highlightBlock(block as HTMLElement);
+      });
+    }
+  }, [ref])
   return (
     <div className="max-w-6xl mx-auto">
       <div
+        ref={ref}
         className={markdownStyles['markdown']}
         dangerouslySetInnerHTML={{ __html: content }}
       />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,15 +5,11 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <script defer src="https://hm.baidu.com/hm.js?1d2bb1da3654d9c3c8d42546c6e8691e"></script>
-        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.5.0/styles/atom-one-dark.min.css" />
-
         <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.5.0/highlight.min.js"></script>
-        <script>hljs.initHighlightingOnLoad();</script>
       </Html>
     )
   }
